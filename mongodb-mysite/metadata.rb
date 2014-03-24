b1fbdde5a5bd80973cf57bcbdb85b816f4b6aea2
cbbarclay <cbbarclay@hotmail.com>
2014-03-24 08:41:08 -0700

    Create metadata.rb

---
+++ mongodb-mysite/metadata.rb
@@ -0,0 +1,7 @@
+name        "mongodb-mysite"
+description 'Example cookbook for MongoDB Berkshelf based install'
+maintainer  "AWS OpsWorks"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends 'mongodb'
